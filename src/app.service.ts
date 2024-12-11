5353bbbd074e9fb4e65e99e1c308049bb47b255c
Karthik Avula <96226768+avula-karthik@users.noreply.github.com>
2024-12-11 16:35:25 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Test Test';
+    return 'Hello World! Test Test v3';
   }
 }
