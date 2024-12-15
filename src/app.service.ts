9f7a31d6ed9070e0413f71d547d1adb0a187537a
Karthik Avula <96226768+avula-karthik@users.noreply.github.com>
2024-12-15 12:32:31 +0530

    final !

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World ! latest change ! 12:25 PM';
+    return 'Hello World !';
   }
 }
