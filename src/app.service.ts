1330e7659838af521b359d84e379dd33245175fa
Karthik Avula <karthik.a@veltris.com>
2024-12-15 13:06:02 +0530

    added v1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello AWS - CI/CD';
+    return 'Hello world v2 deployed !';
   }
 }
